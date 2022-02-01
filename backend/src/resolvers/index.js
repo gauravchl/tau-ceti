1090631072b7cdd7691bbc8d4f2f8699603c7292
Gaurav Chikhale <gauravchl@users.noreply.github.com>
2022-02-01 11:42:39 +0530

    Add GraphQL Query/Mutation and Subscription

--- backend/src/resolvers/index.js
+++ backend/src/resolvers/index.js
@@ -1,9 +1,31 @@
+import { PubSub } from "graphql-subscriptions";
 import Reviews from "../models/reviews";
+import Products from "../models/products";
+
+const pubsub = new PubSub();
 
 const resolvers = {
+  Subscription: {
+    reviewAdded: {
+      subscribe: () => pubsub.asyncIterator(["REVIEW_ADDED"]),
+    },
+  },
+
   Query: {
     reviews() {
       return Reviews.findAll();
+    },
+    products() {
+      return Products.findAll();
+    },
+  },
+  Mutation: {
+    addReview: async (parent, args) => {
+      const { review, rating } = args;
+      const item = await Reviews.create({ rating, review });
+      const newReview = item.toJSON();
+      pubsub.publish("REVIEW_ADDED", { reviewAdded: newReview });
+      return newReview;
     },
   },
 };
--- backend/src/schema/index.js
+++ backend/src/schema/index.js
@@ -2,11 +2,33 @@ import { gql } from "apollo-server-express";
 
 const typeDefs = gql`
   type Review {
+    rating: Int
+    review: String
     id: Int
+    productId: Int
+    createdAt: String
+    updatedAt: String
+  }
+
+  type Product {
+    id: Int
+    title: String
+    author: String
+    createdAt: String
+    updatedAt: String
   }
 
   type Query {
     reviews: [Review]
+    products: [Product]
+  }
+
+  type Mutation {
+    addReview(rating: Int!, review: String!, productId: Int!): Review
+  }
+
+  type Subscription {
+    reviewAdded: Review
   }
 `;
 

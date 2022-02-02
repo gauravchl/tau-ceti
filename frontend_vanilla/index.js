efd681497e877fd3d652c69fca68931b46f1ce5e
Gaurav Chikhale <gauravchl@users.noreply.github.com>
2022-02-02 15:16:10 +0530

    Update production backend url

--- frontend_vanilla/index.js
+++ frontend_vanilla/index.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = "http://localhost:8080";
+const BACKEND_URL = "http://159.223.139.179";
 
 const fetchData = async () => {
   const res1 = await fetch(`${BACKEND_URL}/products`);
